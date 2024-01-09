d5b4a875f6801479776d650324170a4d490fdd42
winkerVSbecks <varunvachhar@gmail.com>
2024-01-09 10:58:19 -0500

    maze

--- src/sketches/chaikin-paths.ts
+++ src/sketches/chaikin-paths.ts
@@ -3,9 +3,29 @@ import type { Sketch, SketchProps, SketchSettings } from 'ssam';
 import { generateColorRamp, colorToCSS } from 'rampensau';
 import Random from 'canvas-sketch-util/random';
 import { generateColors } from '../subtractive-color';
-import { clamp, lerp, lerpFrames } from 'canvas-sketch-util/math';
+import { clamp, lerp, lerpFrames, mapRange } from 'canvas-sketch-util/math';
 import smooth from 'chaikin-smooth';
 import eases from 'eases';
+
+const generateConfig = () => {
+  const ordered = Random.chance(0.5);
+  return {
+    cycles: 4,
+    pathCount: 50,
+    ordered,
+    uniform: Random.chance(0.5),
+    colorPalette: ordered ? false : Random.chance(0.5),
+  };
+};
+
+let config = generateConfig();
+
+interface Path {
+  points: Line;
+  color: string;
+  length: number;
+  thickness: number;
+}
 
 export const sketch = ({ wrap, context, width, height }: SketchProps) => {
   if (import.meta.hot) {
@@ -13,39 +33,66 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
     import.meta.hot.accept(() => wrap.hotReload());
   }
 
-  let hStart = Random.rangeFloor(0, 360);
-  const colors = false //Random.chance(0.5)
-    ? generateColors()
-    : generateColors2(hStart);
+  let colors: string[];
 
-  const bg = colors.pop()!;
-  const margin = 20;
+  let bg: string;
+  const margin = 150;
 
-  const paths = Array.from({ length: 50 }).map(() => {
-    const yMin = Random.range(0, height);
-    const yMax = clamp(Random.range(0, yMin + 0.4 * height), 0, height);
+  let paths: Path[] = [];
 
-    const points = generatePath([-margin, width + margin], [yMin, yMax], 50);
-    return {
-      points,
-      color: Random.pick(colors),
-      length: getLength(points),
-      thickness: 40, //Random.pick([20, 40, 60]),
-    };
-  });
-
-  wrap.render = ({ width, height, playhead }: SketchProps) => {
+  wrap.render = ({
+    width,
+    height,
+    playhead,
+    frame,
+    totalFrames,
+  }: SketchProps) => {
     context.fillStyle = bg;
     context.fillRect(0, 0, width, height);
     context.lineCap = 'butt';
     context.lineJoin = 'round';
 
-    const t = eases.quadInOut(playhead);
+    if (frame % (totalFrames / config.cycles) === 0) {
+      config = generateConfig();
+
+      colors = config.colorPalette
+        ? generateColors()
+        : generateColorsFromRampensau(config.pathCount + 1);
+      bg = colors.pop()!;
+
+      paths = Array.from({ length: config.pathCount }).map((_, idx) => {
+        const yMin = config.ordered
+          ? mapRange((idx + 1) / config.pathCount, 0, 1, 0, height)
+          : Random.range(0, height);
+        const yMax = clamp(Random.range(0, yMin + 0.4 * height), 0, height);
+
+        const points = generatePath(
+          [-margin, width + margin],
+          [yMin, yMax],
+          10
+        );
+
+        return {
+          points,
+          color: config.ordered ? colors[idx] : Random.pick(colors),
+          length: getLength(points),
+          thickness: config.uniform ? 40 : Random.pick([40, 60, 80, 120, 180]),
+        };
+      });
+    }
+
+    const cyclePlayhead = (playhead * config.cycles) % 1;
+
+    const t = lerpFrames(
+      [0, 1, 0],
+      eases.quadInOut(cyclePlayhead),
+      []
+    ) as unknown as number;
 
     paths.forEach(({ points, color, length, thickness }) => {
       const [start, ...rest] = points;
       context.lineWidth = thickness;
-      context.setLineDash([length, length]);
+      context.setLineDash([length]);
 
       context.beginPath();
       context.moveTo(...start);
@@ -53,7 +100,8 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
         context.lineTo(x, y);
       });
       context.strokeStyle = color;
-      context.lineDashOffset = (1 - t) * length * 2;
+      context.lineDashOffset =
+        cyclePlayhead <= 0.5 ? length * (1 - t) : length + length * t;
       context.stroke();
     });
   };
@@ -64,7 +112,7 @@ export const settings: SketchSettings = {
   dimensions: [1080, 1080],
   pixelRatio: window.devicePixelRatio,
   animate: true,
-  duration: 6_000,
+  duration: 6_000 * config.cycles,
   playFps: 60,
   exportFps: 60,
   framesFormat: ['mp4'],
@@ -108,12 +156,11 @@ function generatePath(
 }
 
 // Colors
-function generateColors2(hStart: number) {
-  const s = 0.6; // 0.2, 0.4, 0.6, 0.8
-  const l = 0.6; // 0.2, 0.4, 0.6, 0.8
+function generateColorsFromRampensau(count: number) {
+  const hStart = Random.rangeFloor(0, 360);
 
   const colors = generateColorRamp({
-    total: 24,
+    total: count,
     hStart,
     hEasing: (x) => x,
     hCycles: 1 / 3,
---
+++ src/sketches/chaotic-pendulums.ts
@@ -0,0 +1,216 @@
+import { ssam } from 'ssam';
+import type { Sketch, SketchProps, SketchSettings } from 'ssam';
+import { generateColorRamp, colorToCSS } from 'rampensau';
+import Random from 'canvas-sketch-util/random';
+import { drawCircle, drawLine, drawPath } from '@daeinc/draw';
+
+interface Pendulum {
+  r1: number;
+  r2: number;
+  m1: number;
+  m2: number;
+  a1: number;
+  a2: number;
+  a1Vel: number;
+  a2Vel: number;
+  g: number;
+  trail1: Line;
+  trail2: Line;
+  pathLength: number;
+  color1: string;
+  color2: string;
+}
+
+const config = {
+  pendulumCount: 25,
+};
+
+export const sketch = ({ wrap, context }: SketchProps) => {
+  if (import.meta.hot) {
+    import.meta.hot.dispose(() => wrap.dispose());
+    import.meta.hot.accept(() => wrap.hotReload());
+  }
+
+  const colors = generateColors(config.pendulumCount + 2);
+
+  const bg = colors.pop()!;
+
+  let pendulums: Pendulum[] = [];
+
+  wrap.render = ({ width, height, frame }: SketchProps) => {
+    context.fillStyle = bg;
+    context.fillRect(0, 0, width, height);
+
+    if (frame === 0) {
+      pendulums = Array.from({
+        length: config.pendulumCount,
+      }).map((_, idx) => {
+        return {
+          r1: 25 + ((idx + 1) / config.pendulumCount) * height * 0.5,
+          r2: 25 + ((idx + 1) / config.pendulumCount) * height * 0.5,
+          m1: 10,
+          m2: 10,
+          a1: Math.PI * 0.5,
+          a2: Math.PI * 0.75,
+          a1Vel: 0,
+          a2Vel: 0,
+          g: 1,
+          trail1: [],
+          trail2: [],
+          pathLength: 25,
+          color1: colors[idx],
+          color2: colors[idx + 1],
+        };
+      });
+    }
+
+    pendulums.forEach((pendulum) => {
+      const [location1, location2] = updateLocations(pendulum);
+
+      context.save();
+      context.translate(width / 2, 0);
+      drawPendulum(context, pendulum, [location1, location2], false);
+      context.restore();
+
+      updateAccAndVel(pendulum, true);
+    });
+  };
+};
+
+/**
+ * Pendulum Math
+ */
+function updateLocations(pendulum: Pendulum) {
+  const location1: Point = [
+    pendulum.r1 * Math.sin(pendulum.a1),
+    pendulum.r1 * Math.cos(pendulum.a1),
+  ];
+  const location2: Point = [
+    location1[0] + pendulum.r2 * Math.sin(pendulum.a2),
+    location1[1] + pendulum.r2 * Math.cos(pendulum.a2),
+  ];
+
+  return [location1, location2];
+}
+
+function drawPendulum(
+  context: CanvasRenderingContext2D,
+  pendulum: Pendulum,
+  [location1, location2]: [Point, Point],
+  debug?: boolean
+) {
+  if (debug) {
+    // Draw Pendulum
+    context.strokeStyle = '#fff';
+    drawLine(context, [0, 0], location1);
+    context.stroke();
+    drawLine(context, location1, location2);
+    context.stroke();
+    drawCircle(context, [0, 0], 2);
+    context.stroke();
+    context.fillStyle = '#fff';
+    drawCircle(context, location1, 12);
+    context.fill();
+    drawCircle(context, location2, 12);
+    context.fill();
+  }
+
+  // Draw Trails
+  pendulum.trail1.push(location1);
+  pendulum.trail2.push(location2);
+
+  if (pendulum.trail1.length > pendulum.pathLength) {
+    pendulum.trail1.shift();
+  }
+  if (pendulum.trail2.length > pendulum.pathLength) {
+    pendulum.trail2.shift();
+  }
+
+  context.lineWidth = 8;
+  context.lineCap = 'round';
+  // Motion path of top rod
+  context.strokeStyle = pendulum.color1;
+  drawPath(context, pendulum.trail1);
+  context.stroke();
+  // Motion path of bottom rod
+  context.strokeStyle = pendulum.color2;
+  drawPath(context, pendulum.trail2);
+  context.stroke();
+}
+
+function updateAccAndVel(pendulum: Pendulum, airFriction?: boolean) {
+  // Update acceleration ➡ velocity ➡ angle
+  const a1Acc = angularAccTop(pendulum);
+  const a2Acc = angularAccBottom(pendulum);
+
+  pendulum.a1Vel += a1Acc;
+  pendulum.a2Vel += a2Acc;
+  pendulum.a1 += pendulum.a1Vel;
+  pendulum.a2 += pendulum.a2Vel;
+
+  if (airFriction) {
+    pendulum.a1Vel *= 0.995; //0.9999;
+    pendulum.a2Vel *= 0.995; //0.9999;
+  }
+}
+
+// Colors
+function generateColors(count: number) {
+  const hStart = Random.rangeFloor(0, 360);
+
+  const colors = generateColorRamp({
+    total: count,
+    hStart,
+    hEasing: (x) => x,
+    hCycles: 1, // / 3,
+    sRange: [0.2, 0.8],
+    lRange: [0.2, 0.8],
+  })
+    .reverse()
+    .map((color) => colorToCSS(color, 'hsl'));
+
+  return colors;
+}
+
+// angular acceleration of top rod
+function angularAccTop({ r1, r2, m1, m2, a1, a2, a1Vel, a2Vel, g }: Pendulum) {
+  const num1 = -g * (2 * m1 + m2) * Math.sin(a1);
+  const num2 = -m2 * g * Math.sin(a1 - 2 * a2);
+  const num3 = -2 * Math.sin(a1 - a2) * m2;
+  const num4 = a2Vel * a2Vel * r2 + a1Vel * a1Vel * r1 * Math.cos(a1 - a2);
+  const den = r1 * (2 * m1 + m2 - m2 * Math.cos(2 * a1 - 2 * a2));
+  return (num1 + num2 + num3 * num4) / den;
+}
+
+// angular acceleration of bottom rod
+function angularAccBottom({
+  r1,
+  r2,
+  m1,
+  m2,
+  a1,
+  a2,
+  a1Vel,
+  a2Vel,
+  g,
+}: Pendulum) {
+  const num1 = 2 * Math.sin(a1 - a2);
+  const num2 = a1Vel * a1Vel * r1 * (m1 + m2);
+  const num3 = g * (m1 + m2) * Math.cos(a1);
+  const num4 = a2Vel * a2Vel * r2 * m2 * Math.cos(a1 - a2);
+  const den = r2 * (2 * m1 + m2 - m2 * Math.cos(2 * a1 - 2 * a2));
+  return (num1 * (num2 + num3 + num4)) / den;
+}
+
+export const settings: SketchSettings = {
+  mode: '2d',
+  dimensions: [1080, 1080],
+  pixelRatio: window.devicePixelRatio,
+  animate: true,
+  duration: 20_000,
+  playFps: 60,
+  exportFps: 60,
+  framesFormat: ['mp4'],
+};
+
+ssam(sketch as Sketch, settings);
