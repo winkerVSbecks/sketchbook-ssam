58b9da358f390a79afef8c07b585397433867003
winkerVSbecks <varunvachhar@gmail.com>
2024-01-27 14:34:01 -0500

    boids

--- src/sketches/boids-pipe.ts
+++ src/sketches/boids-pipe.ts
@@ -4,11 +4,11 @@ import type { Sketch, SketchProps, SketchSettings } from 'ssam';
 import { mapRange } from 'canvas-sketch-util/math';
 import { interpolate, parse, formatCss, Color } from 'culori';
 import { Vector } from 'p5';
-import { drawPath } from '@daeinc/draw';
+import { snapBy, snapToArray } from '@daeinc/math';
 import { generateColors } from '../subtractive-color';
 
 const config = {
-  resolution: 64,
+  resolution: 64 * 2,
   neighbourDist: 60,
   desiredSeparation: 40,
 };
@@ -26,7 +26,7 @@ const trailColorMap = (t: number) => formatCss(colorSale(t));
 const headColor1 = formatCss({ ...parse(headColor), alpha: 1 } as Color);
 const headColor2 = formatCss({ ...parse(headColor), alpha: 0 } as Color);
 const headColorScale = interpolate([headColor1, headColor2]);
-const headColorMap = (t: number) => formatCss(colorSale(t));
+const headColorMap = (t: number) => formatCss(headColorScale(t));
 
 interface Boid {
   acceleration: Vector;
@@ -46,17 +46,37 @@ interface Node {
   occupied: boolean;
 }
 
-export const sketch = ({ wrap, context, width, height }: SketchProps) => {
+const start = new Date(
+  'Sat Jan 27 2024 10:38:02 GMT-0500 (Eastern Standard Time)'
+);
+
+export const sketch = ({
+  wrap,
+  context,
+  width,
+  height,
+  canvas,
+}: SketchProps) => {
   if (import.meta.hot) {
     import.meta.hot.dispose(() => wrap.dispose());
     import.meta.hot.accept(() => wrap.hotReload());
+
+    // import.meta.hot.on('ssam:timelapse-changed', () => {
+    //   import.meta.hot?.send('ssam:timelapse-newframe', {
+    //     image: canvas.toDataURL(),
+    //   });
+    // });
   }
 
   let flock: Boid[];
   let grid: Node[] = [];
+  let gridX: number[] = [];
+  let gridY: number[] = [];
+  let bounds: [number, number, number, number];
 
-  const handleScreenBoundaries = handleBoundaries(width, height);
+  let handleBoundaries: (boid: Boid) => void;
   const margin = 0.05 * width;
+  let step = (width - 2 * margin) / config.resolution;
 
   const xyToWorld = (x: number, y: number) => [
     mapRange(x, 0, config.resolution - 1, margin, width - margin),
@@ -75,6 +95,16 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
     grid.forEach((node) => {
       node.occupied = false;
     });
+    gridX = grid.map((node) => node.x);
+    gridY = grid.map((node) => node.y);
+
+    const xMin = Math.min(...gridX);
+    const xMax = Math.max(...gridX);
+    const yMin = Math.min(...gridY);
+    const yMax = Math.max(...gridY);
+    bounds = [xMin, yMin, xMax, yMax];
+
+    handleBoundaries = handleGridBoundaries(bounds);
 
     flock = Array.from({ length: 128 }).map(() => {
       const node = Random.pick(grid.filter((node) => !node.occupied));
@@ -99,9 +129,9 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
 
     flock.forEach((boid) => {
       move(flock, boid);
-      update(boid);
-      handleScreenBoundaries(boid);
-      render(boid, context, width, height, playhead);
+      update(boid, gridX, gridY, step, bounds);
+      handleBoundaries(boid);
+      render(boid, context, width, height, playhead, bounds);
 
       if (playhead > 0.8) {
         boid.trailLength = Math.max(
@@ -111,6 +141,21 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
         boid.trail.shift();
       }
     });
+
+    // // Timer
+    // const now = new Date();
+    // const timeElapsed = now.getTime() - start.getTime();
+    // const timeElapsedSeconds = Math.floor(timeElapsed / 1000);
+    // const mins = `${Math.floor(timeElapsedSeconds / 60)}`.padStart(2, '0');
+    // const secondsRemainder = `${timeElapsedSeconds % 60}`.padStart(2, '0');
+
+    // context.fillStyle = bg;
+    // context.fillRect(margin - 4, margin - 4, 112, 40);
+    // context.font = `600 32px monospace`;
+    // context.fillStyle = '#222';
+    // context.textAlign = 'left';
+    // context.textBaseline = 'top';
+    // context.fillText(`${mins}:${secondsRemainder}`, margin, margin);
   };
 };
 
@@ -130,7 +175,7 @@ function boidOf(x: number, y: number) {
     maxSpeed: 3 * scale,
     maxForce: 0.1 * scale,
     trail: [],
-    trailLength: 30, // Random.range(25, 60),
+    trailLength: Random.rangeFloor(10, 30), // 10, // Random.range(25, 60),
     color: Random.pick(colors),
   };
 }
@@ -157,11 +202,32 @@ function move(boids: Boid[], boid: Boid) {
 /**
  * Update the location of the boid
  */
-function update(boid: Boid) {
+function update(
+  boid: Boid,
+  gridX: number[],
+  gridY: number[],
+  step: number,
+  [xMin, yMin, xMax, yMax]: [number, number, number, number]
+) {
   boid.velocity.add(boid.acceleration);
   // Limit speed
   boid.velocity.limit(boid.maxSpeed);
-  boid.position.add(boid.velocity);
+
+  const heading = boid.velocity.heading();
+  // snap heading to 45 degrees increments
+  const snappedHeading = snapBy(heading, Math.PI / 4);
+  const velocity = Vector.fromAngle(snappedHeading).mult(step);
+  boid.position.add(velocity);
+
+  // // snap to grid
+  // const velocity = boid.velocity.copy().normalize().mult(step);
+  // boid.position.add(velocity);
+  // if (boid.position.x > xMin && boid.position.x < xMax) {
+  //   boid.position.x = snapToArray(boid.position.x, gridX);
+  // }
+  // if (boid.position.y > yMin && boid.position.y < yMax) {
+  //   boid.position.y = snapToArray(boid.position.y, gridY);
+  // }
 
   // Trail
   boid.trail.push([boid.position.x, boid.position.y]);
@@ -183,13 +249,14 @@ function render(
   context: CanvasRenderingContext2D,
   width: number,
   height: number,
-  playhead: number = 0
+  playhead: number = 0,
+  bounds: [number, number, number, number]
 ) {
   context.fillStyle = boid.color;
   context.strokeStyle = boid.color;
   context.lineWidth = boid.r;
 
-  const chunks = splitPath(boid, width, height);
+  const chunks = splitPath(boid, bounds);
 
   // Draw trail
   chunks.forEach((chunk) => {
@@ -203,22 +270,12 @@ function render(
     playhead > 0.9
       ? headColorMap(mapRange(playhead, 0.9, 0.95, 0, 1, true))
       : headColor;
-  const theta = boid.velocity.heading() + Math.PI / 2;
-  context.save();
-  context.translate(boid.position.x, boid.position.y);
-  context.rotate(theta);
-  context.beginPath();
-  drawPath(context, drawEquilateralTriangle([0, 0], boid.r * 2));
-  context.fill();
-  context.restore();
-}
-
-function drawEquilateralTriangle([x, y]: number[], size: number) {
-  const a = [x, y - size];
-  const b = [x + (size * Math.sqrt(3)) / 2, y + (size * Math.sqrt(3)) / 2];
-  const c = [x - (size * Math.sqrt(3)) / 2, y + (size * Math.sqrt(3)) / 2];
-
-  return [a, b, c];
+  context.fillRect(
+    boid.position.x - boid.r,
+    boid.position.y - boid.r,
+    boid.r * 2,
+    boid.r * 2
+  );
 }
 
 function drawGradientPath(context: CanvasRenderingContext2D, path: Point[]) {
@@ -326,26 +383,28 @@ function seek(target: Vector, boid: Boid) {
   return steer;
 }
 
-/**
- * Wrap the boid around canvas boundaries
- */
-function handleBoundaries(width: number, height: number) {
+function handleGridBoundaries([xMin, yMin, xMax, yMax]: [
+  number,
+  number,
+  number,
+  number
+]) {
   return (boid: Boid) => {
     // Left
-    if (boid.position.x < -boid.r) {
-      boid.position.x = width + boid.r;
+    if (boid.position.x < xMin) {
+      boid.position.x = xMax;
     }
     // Top
-    if (boid.position.y < -boid.r) {
-      boid.position.y = height + boid.r;
+    if (boid.position.y < yMin) {
+      boid.position.y = yMax;
     }
     // Right
-    if (boid.position.x > width + boid.r) {
-      boid.position.x = -boid.r;
+    if (boid.position.x > xMax) {
+      boid.position.x = xMin;
     }
     // Bottom
-    if (boid.position.y > height + boid.r) {
-      boid.position.y = -boid.r;
+    if (boid.position.y > yMax) {
+      boid.position.y = yMin;
     }
   };
 }
@@ -353,16 +412,16 @@ function handleBoundaries(width: number, height: number) {
 /**
  * Split path into chunks that are on or off canvas
  */
-function splitPath(boid: Boid, width: number, height: number) {
+function splitPath(
+  boid: Boid,
+  [xMin, yMin, xMax, yMax]: [number, number, number, number]
+) {
   let prevOffCanvas = false;
 
   return boid.trail.reduce<Point[][]>(
     (acc, pt) => {
       const offCanvas =
-        pt[0] < -boid.r ||
-        pt[0] > width + boid.r ||
-        pt[1] < -boid.r ||
-        pt[1] > height + boid.r;
+        pt[0] < xMin || pt[0] > xMax || pt[1] < yMin || pt[1] > yMax;
 
       if (offCanvas !== prevOffCanvas) {
         acc.push([]);
@@ -382,8 +441,8 @@ export const settings: SketchSettings = {
   pixelRatio: window.devicePixelRatio,
   animate: true,
   duration: 20_000,
-  playFps: 60,
-  exportFps: 60,
+  playFps: 30,
+  exportFps: 30,
   framesFormat: ['mp4'],
 };
 
