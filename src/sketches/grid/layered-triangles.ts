625b6c95ee8176dced7d240617d5e5ddf7b9bc61
winkerVSbecks <varunvachhar@gmail.com>
2025-01-07 12:40:30 -0500

    poisson-disc grid and layered triangles

--- src/sketches/grid/layered-triangles.ts
+++ src/sketches/grid/layered-triangles.ts
@@ -3,6 +3,9 @@ import type { Sketch, SketchProps, SketchSettings } from 'ssam';
 import PoissonDiskSampling from 'poisson-disk-sampling';
 import Random from 'canvas-sketch-util/random';
 import * as tome from 'chromotome';
+import { Delaunay } from 'd3-delaunay';
+import { drawPath } from '@daeinc/draw';
+import { clrs } from '../../colors/clrs';
 
 type Region = {
   id: number;
@@ -14,16 +17,39 @@ type Region = {
 type Grid = (number | null)[][];
 
 const config = {
-  res: [6, 4],
+  // res: [6, 4],
+  res: [6 * 2, 4 * 2],
   gap: 0.02,
   minDistance: 5,
   maxDistance: 20,
   size: 2,
+  layers: 12,
+  layerOpacity: 10,
+  palette: Random.pick(['clrs', 'tome']), // tome or clrs
 };
 
-let { colors, background, stroke, name } = tome.get(/* 'spatial03' */);
-stroke ??= Random.pick(colors);
-console.log(name, stroke);
+const adjustOpacity = (c: string) =>
+  `rgb(from ${c} r g b / ${config.layerOpacity}%)`;
+
+function generateColors() {
+  if (config.palette === 'clrs') {
+    const palette = Random.pick(clrs);
+    const background = palette.shift()!;
+    const colors = Random.shuffle(palette);
+    const stroke = adjustOpacity(colors.shift());
+
+    return { background, colors, stroke };
+  } else {
+    const { colors, background, stroke, name } = tome.get();
+    console.log(name);
+    return {
+      colors,
+      background,
+      stroke: adjustOpacity(stroke || Random.pick(colors)),
+    };
+  }
+}
+const { background, colors, stroke } = generateColors();
 
 export const sketch = ({ wrap, context, width, height }: SketchProps) => {
   function generateAreas(rows: number, cols: number): Region[] {
@@ -93,23 +119,37 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
       const points = p.fill().map(([x, y]) => [offset[0] + x, offset[1] + y]);
       pointCount += points.length;
 
-      context.fillStyle = color;
-      const s = config.size * 1.5;
-      points.forEach(([x, y]) => {
-        context.fillRect(x - s / 2, y - s / 2, s, s);
+      const delaunay = Delaunay.from(points as any);
+      const polygons = [...delaunay.trianglePolygons()];
+      triangleCount += polygons.length;
+
+      context.strokeStyle = adjustOpacity(Random.pick(colors));
+
+      context.lineWidth = 1;
+      polygons.forEach((polygon) => {
+        drawPath(context, polygon);
+        context.stroke();
       });
+
+      // context.fillStyle = color;
+      // const s = config.size * 1.5;
+      // points.forEach(([x, y]) => {
+      //   context.fillRect(x - s / 2, y - s / 2, s, s);
+      // });
     }
 
     // fillPoints([width, height], [0, 0], Random.pick(colors));
 
-    generateAreas(config.res[1], config.res[0]).forEach((r) => {
-      const gW = r.width * w - gap;
-      const gH = r.height * h - gap;
-      const gX = gap / 2 + r.x * w + gap / 2;
-      const gY = gap / 2 + r.y * h + gap / 2;
+    for (let layer = 0; layer < config.layers; layer++) {
+      generateAreas(config.res[1], config.res[0]).forEach((r) => {
+        const gW = r.width * w - gap;
+        const gH = r.height * h - gap;
+        const gX = gap / 2 + r.x * w + gap / 2;
+        const gY = gap / 2 + r.y * h + gap / 2;
 
-      fillPoints([gW, gH], [gX, gY], stroke);
-    });
+        fillPoints([gW, gH], [gX, gY], stroke);
+      });
+    }
 
     console.log(
       new Intl.NumberFormat('en-CA', { maximumSignificantDigits: 3 }).format(
