a50a1e7a173d89eedf6f095fdfcb04c85754aa95
winkerVSbecks <varunvachhar@gmail.com>
2026-01-03 17:25:07 -0500

    2026.01.03-17.25.07

--- src/sketches/grey-goo/genuary.ts
+++ src/sketches/grey-goo/genuary.ts
@@ -1,5 +1,6 @@
 import { ssam } from 'ssam';
 import type { Sketch, SketchProps, SketchSettings } from 'ssam';
+import { mapRange } from 'canvas-sketch-util/math';
 import Random from 'canvas-sketch-util/random';
 import pack from 'pack-spheres';
 import { logColors } from '../../colors';
@@ -20,16 +21,38 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
   Random.setSeed(seed);
   console.log('Seed:', seed);
 
-  wrap.render = ({ playhead }) => {
-    context.fillStyle = bg;
-    context.fillRect(0, 0, width, height);
+  context.fillStyle = bg;
+  context.fillRect(0, 0, width, height);
 
-    context.fillStyle = fg;
-    // draw text in center
-    context.font = `${width / 10}px sans-serif`;
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    context.fillText('Grey Goo', width / 2, height / 2);
+  context.fillStyle = fg;
+  // draw text in center
+  context.font = `bold ${width / 10}px sans-serif`;
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText('GENUARY', width / 2, height / 2);
+
+  const margin = width * 0.05;
+  const scale = width - margin * 2;
+
+  const shapes = pack({
+    dimensions: 2,
+    padding: 0.0025,
+    // minRadius: 0.03125,
+    // maxRadius: 0.75,
+  });
+
+  const circles = shapes.map((shape: any) => ({
+    x: mapRange(shape.position[0], -1, 1, margin, width - margin),
+    y: mapRange(shape.position[1], -1, 1, margin, height - margin),
+    r: (shape.radius * scale) / 2,
+  }));
+
+  wrap.render = ({ playhead }) => {
+    circles.forEach((c: any) => {
+      context.beginPath();
+      context.arc(c.x, c.y, c.r, 0, Math.PI * 2);
+      context.stroke();
+    });
   };
 };
 
