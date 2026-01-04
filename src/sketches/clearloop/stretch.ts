2c20128a0cb668984f476133a36964505d99bad4
winkerVSbecks <varunvachhar@gmail.com>
2026-01-04 17:16:13 -0500

    2026.01.04-17.16.13

--- src/sketches/clearloop/stretch.ts
+++ src/sketches/clearloop/stretch.ts
@@ -7,104 +7,39 @@ import { logColors } from '../../colors';
 
 const config = {
   res: [13, 15],
-  stretch: 3,
+  xStretch: 5,
+  yStretch: 2,
 };
-
-// const bg = '#111';
-// const fg = '#fefefe';
-
-// const palette = ColorPaletteGenerator.generate(
-//   { l: Random.range(0, 1), c: Random.range(0, 0.4), h: Random.range(0, 360) },
-//   Random.pick([
-//     'analogous',
-//     'complementary',
-//     'triadic',
-//     'tetradic',
-//     'splitComplementary',
-//     'tintsShades',
-//   ]),
-//   {
-//     style: Random.pick(['default', 'square', 'triangle', 'circle', 'diamond']),
-//     modifiers: {
-//       sine: Random.range(-1, 1),
-//       wave: Random.range(-1, 1),
-//       zap: Random.range(-1, 1),
-//       block: Random.range(-1, 1),
-//     },
-//   }
-// ).map((c) => formatCss(oklch({ mode: 'oklch', ...c })));
-
-// logColors(palette);
 
 const palette = ['#367565', '#43529F', '#E06D3A', '#EBA98A', '#EBE4C8'];
 const bg = palette.pop()!;
 
-const layers = [
-  {
-    cell: (
-      context: CanvasRenderingContext2D,
-      x: number,
-      y: number,
-      w: number,
-      h: number
-    ) => {
-      context.fillStyle = palette[0];
-      context.fillRect((x + 1) * w, y * h, w * config.stretch, h);
-      context.fillRect(x * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1 + config.stretch) * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1) * w, (y + 3) * h, w * config.stretch, h);
-    },
-    xStep: 1 + config.stretch,
-  },
-  {
-    cell: (
-      context: CanvasRenderingContext2D,
-      x: number,
-      y: number,
-      w: number,
-      h: number
-    ) => {
-      context.fillStyle = palette[1];
-      context.fillRect((x + 1) * w, y * h, w * config.stretch, h);
-      context.fillRect(x * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1 + config.stretch) * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1) * w, (y + 3) * h, w * config.stretch, h);
-    },
-    xStep: 1 + config.stretch,
-  },
-  {
-    cell: (
-      context: CanvasRenderingContext2D,
-      x: number,
-      y: number,
-      w: number,
-      h: number
-    ) => {
-      context.fillStyle = palette[2];
-      context.fillRect((x + 1) * w, y * h, w * config.stretch, h);
-      context.fillRect(x * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1 + config.stretch) * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1) * w, (y + 3) * h, w * config.stretch, h);
-    },
-    xStep: 1 + config.stretch,
-  },
-  {
-    cell: (
-      context: CanvasRenderingContext2D,
-      x: number,
-      y: number,
-      w: number,
-      h: number
-    ) => {
-      context.fillStyle = palette[3];
-      context.fillRect((x + 1) * w, y * h, w * config.stretch, h);
-      context.fillRect(x * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1 + config.stretch) * w, (y + 1) * h, w, h * 2);
-      context.fillRect((x + 1) * w, (y + 3) * h, w * config.stretch, h);
-    },
-    xStep: 1 + config.stretch,
-  },
-];
+const drawCell = (
+  context: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  color: string
+) => {
+  context.fillStyle = color;
+  context.fillRect((x + 1) * w, y * h, w * config.xStretch, h);
+  context.fillRect(x * w, (y + 1) * h, w, h * config.yStretch);
+  context.fillRect(
+    (x + 1 + config.xStretch) * w,
+    (y + 1) * h,
+    w,
+    h * config.yStretch
+  );
+  context.fillRect(
+    (x + 1) * w,
+    (y + 1 + config.yStretch) * h,
+    w * config.xStretch,
+    h
+  );
+};
+const xStep = 1 + config.xStretch;
+const yStep = 1 + config.yStretch;
 
 export const sketch = ({ wrap, context, width, height }: SketchProps) => {
   if (import.meta.hot) {
@@ -123,10 +58,9 @@ export const sketch = ({ wrap, context, width, height }: SketchProps) => {
   wrap.render = () => {
     context.fillStyle = bg;
     context.fillRect(0, 0, width, height);
-
-    layers.forEach((layer, i) => {
-      for (let x = 0; x < config.res[0]; x += layer.xStep) {
-        layer.cell(context, x, i * 3, w, h);
+    palette.forEach((color, i) => {
+      for (let x = 0; x < config.res[0]; x += xStep) {
+        drawCell(context, x, i * yStep, w, h, color);
       }
     });
   };
